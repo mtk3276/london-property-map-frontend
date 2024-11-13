abb4093fb58ceebadd46e70869e357e0c9dfaa77
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-13 11:15:45 +0000

    feat: add React Router and implement initial router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-       <h1>London Property Map</h1>
+       <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import routes from './router'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
