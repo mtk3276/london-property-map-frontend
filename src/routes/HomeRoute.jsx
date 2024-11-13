3e491780fd24c75fca4790eeb097a2023fd18f2e
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-13 15:25:36 +0000

    feat: add Home route contents and styles

--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -1,7 +1,33 @@
+import { useMediaQuery } from "react-responsive";
+import "./HomeRoute.css";
+
 export default function HomePage() {
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     return (
         <>
-            <h1>Home</h1>
+            <div className="home">
+                {/* <div className="home__heading"> */}
+                <div className={`
+                    ${isDesktop ? "home__heading__web" : ""} 
+                    ${isTablet ? "home__heading__tablet" : ""}
+                    ${isMobile ? "home__heading__mobile" : ""}
+                    `}>
+                    <span>2023</span>
+                    <span>London</span>
+                    <span>Residential</span>
+                    <span>Property</span>
+                    <span>Prices</span>
+                </div>
+                <span className={`
+                    ${isDesktop ? "sub__text__web" : ""} 
+                    ${isTablet ? "sub__text__tablet" : ""}
+                    ${isMobile ? "sub__text__mobile" : ""}
+                    `}>
+                        Head to my map to see price paid data for 2023.</span>
+            </div>
         </>
     )
 }
